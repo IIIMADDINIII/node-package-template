de107eaf368ad08741966083f7423d0df0cc354f
IIIMADDINIII <martin@elend.name>
2023-04-25 08:29:35 +0200

    Updated dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// Reference Switches so that the const:production and const:development packages are defined
+/// <reference types="rollup-config-iiimaddiniii/switches" />
+
+
 
 export function helloWorld(): string {
   return "Hello World!";
--- switches.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "consts:production" {
-  let production: boolean;
-  export default production;
-}
-declare module "consts:development" {
-  let development: boolean;
-  export default development;
-}
